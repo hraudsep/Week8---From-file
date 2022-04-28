6a8cda475f406455d8520f261fedefae971a5088
Helen Raudsepp <helen.raudsepp@k-print.ee>
2022-04-28 18:37:52 +0300

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomJoke;
+            randomJoke = GetRandomFromFile("chuck.txt");
+
+            Console.WriteLine($"Here a joke for you: {randomJoke}");
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\helen.raudsepp\Samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
